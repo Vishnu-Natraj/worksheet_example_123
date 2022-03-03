dc44adfff44680694cc6fecc2b2788fd530ec3df
Vishnu-Natraj <vishnuunatraj@gmail.com>
2022-03-03 18:02:31 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,11 @@ import { Router } from '@angular/router';
 import { AuthenticationService } from './_services';
 import { User } from './_models';
 
-@Component({ selector: 'app', 
-templateUrl: 'app.component.html' , 
-styleUrls: ['./app.component.scss'],})
+@Component({
+  selector: 'app',
+  templateUrl: 'app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
 
 // @Component({
 //     selector: 'my-sidebar',
@@ -14,20 +16,23 @@ styleUrls: ['./app.component.scss'],})
 //     styleUrls: ['./sidebar.component.scss'],
 //   })
 export class AppComponent {
-    user: User;
-    sidebarExpanded = true;
-    isExpanded="sidebarExpanded"
-    toggleSidebar(){"sidebarExpanded = !sidebarExpanded"}
-    handleSidebarToggle = this.sidebarExpanded;
+  isshow = false;
+  user: User;
+  sidebarExpanded = true;
+  isExpanded = 'sidebarExpanded';
+  toggleSidebar() {
+    'sidebarExpanded = !sidebarExpanded';
+  }
+  handleSidebarToggle = this.sidebarExpanded;
 
-    constructor(
-        private router: Router,
-        private authenticationService: AuthenticationService
-    ) {
-        this.authenticationService.user.subscribe(x => this.user = x);
-    }
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ) {
+    this.authenticationService.user.subscribe((x) => (this.user = x));
+  }
 
-    logout() {
-        this.authenticationService.logout();
-    }
+  logout() {
+    this.authenticationService.logout();
+  }
 }
